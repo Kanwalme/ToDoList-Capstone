f66ec71af9c7175b63b63be47e38fae75ce7b3db
Kanwalme <mehmudkanwal@gmail.com>
2022-09-19 14:19:13 -0500

    entities and dtos

---
+++ ToDoList/src/main/java/com/devmountain/ToDoList/dtos/ToDoDto.java
@@ -0,0 +1,38 @@
+package com.devmountain.ToDoList.dtos;
+
+import com.devmountain.ToDoList.entities.ToDo;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ToDoDto implements Serializable {
+
+    private Long id;
+    private String item;
+    private String date;
+    private boolean completed;
+
+    private UserDto userDto;
+
+    public ToDoDto(ToDo toDo) {
+        if (toDo.getId() != null) {
+            this.id = toDo.getId();
+        }
+        if (toDo.getItem() != null) {
+            this.item = toDo.getItem();
+        }
+        if (toDo.getDate() != null) {
+            this.date = toDo.getDate();
+        }
+        if (toDo.isCompleted()== false) {
+            this.completed = toDo.isCompleted();
+        }
+
+
+    }
+}
---
+++ ToDoList/src/main/java/com/devmountain/ToDoList/entities/ToDo.java
@@ -0,0 +1,48 @@
+package com.devmountain.ToDoList.entities;
+
+import com.devmountain.ToDoList.dtos.ToDoDto;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ToDo")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ToDo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(columnDefinition = "text")
+    private String item;
+
+    @Column
+    private String date;
+
+    @Column
+    private boolean completed;
+
+
+    @ManyToOne
+    @JsonBackReference
+    private User user;
+
+    public ToDo(ToDoDto toDoDto){
+        if (toDoDto.getItem() != null) {
+            this.item = toDoDto.getItem();
+        }
+        if (toDoDto.getDate() != null) {
+            this.date = toDoDto.getDate();
+        }
+        if (toDoDto.isCompleted()== false) {
+            this.completed = toDoDto.isCompleted();
+        }
+    }
+}
+
+
